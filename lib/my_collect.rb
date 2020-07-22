a270edcee2de65298cac4f61b4eaef3ceb123a49
Jason Keeling <s2jason@yahoo.com>
2020-07-22 23:29:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,9 +4,9 @@ def my_collect(array)
   i = 0 
   collection = []
     while i < array.length
+    binding.pry
     collection << yield(array[i])
     i += 1
-    binding.pry
   end
   collection
 end
