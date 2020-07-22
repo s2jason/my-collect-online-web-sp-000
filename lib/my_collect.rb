4699b4b91687bc1926c157530bf0929710400deb
Jason Keeling <s2jason@yahoo.com>
2020-07-22 23:26:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
+require 'pry'
+
 def my_collect(array)
-  do |name|
-  name.split(" ").first
-end
   i = 0 
   collection = []
     while i < array.length
     collection << yield(array[i])
     i += 1
+    binding.pry
   end
   collection
 end
